812d21d0c234d85df47acaed17527e8e9842f746
Aliaksei Rudak <lexarudak@gmail.com>
2023-06-20 14:58:47 +0600

    feat: add settings for first deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import '@/styles/globals.scss';
-import { Inter } from 'next/font/google'
+import "@/styles/globals.scss";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Билетопоиск',
-  description: 'Домашнее заданее "Реакт для Яндекс ШРИ',
-}
+  title: "Билетопоиск",
+  description: 'Домашнее задание "Реакт для Яндекс ШРИ',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
-}) {
+  children: React.ReactNode;
+}): JSX.Element {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
