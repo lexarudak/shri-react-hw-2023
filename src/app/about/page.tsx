1b172d0669e672be14fddcdd6102a345d7a436b5
Aliaksei Rudak <lexarudak@gmail.com>
2023-06-20 20:36:27 +0600

    feat: add about page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,17 @@
+import { Card } from "@/components/Card/Card";
 import styles from "./AboutPage.module.scss";
+import { PAGE_TEXT, TITLE } from "./AboutPage.const";
 
 export default function AboutPage(): JSX.Element {
-  return <section className={styles.page}>About us</section>;
+  const fillPage = (pageText: string[]): JSX.Element[] =>
+    pageText.map((text, ind) => <p key={ind}>{text}</p>);
+
+  return (
+    <section className={styles.page}>
+      <Card>
+        <h2 className={styles.title}>{TITLE}</h2>
+        <>{fillPage(PAGE_TEXT)}</>
+      </Card>
+    </section>
+  );
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,14 @@
 import { MultipleChildren } from "@/model/interface";
 import styles from "./Card.module.scss";
 
-export const Card = ({ children }: MultipleChildren): JSX.Element => {
-  const fillCard = (): JSX.Element[] => children.map((child) => child);
-  return <div className={styles.card}>{fillCard()}</div>;
+export const Card = ({
+  children,
+}: MultipleChildren | { children: JSX.Element }): JSX.Element => {
+  const fillCard = (children: JSX.Element[]): JSX.Element[] =>
+    children.map((child) => child);
+  return (
+    <div className={styles.card}>
+      {Array.isArray(children) ? fillCard(children) : children}
+    </div>
+  );
 };
