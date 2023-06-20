af7c6b8b5890d9f83328127a8399d1a7f08da70b
Aliaksei Rudak <lexarudak@gmail.com>
2023-06-20 12:37:16 +0600

    feat: add basic project settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    additionalData: `@import "src/styles/index.scss";`,
+  },
+  experimental: { appDir: true },
+}
 
 module.exports = nextConfig
