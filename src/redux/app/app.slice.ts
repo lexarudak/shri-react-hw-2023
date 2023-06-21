dcf7933f2316a38780337d60de885a2c35d080d9
Aliaksei Rudak <lexarudak@gmail.com>
2023-06-21 16:40:59 +0600

    fix: fix set cart action

--- src/redux/app/app.slice.ts
+++ src/redux/app/app.slice.ts
@@ -1,11 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { initialState } from "./app.const";
+import { MovieShort } from "@/model/typesAndInterface";
 
 export const appSlice = createSlice({
   name: "appSlice",
   initialState,
   reducers: {
-    setCart(state, action: PayloadAction<string>): void {
+    setCart(state, action: PayloadAction<MovieShort>): void {
       state.cart.push(action.payload);
     },
   },
