25ad87becec5a369a5fda7184f587d512b3a75b2
Aliaksei Rudak <lexarudak@gmail.com>
2023-06-24 19:29:30 +0600

    refactor: add text mixins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,18 @@ import { Header } from "./components/Header/Header";
 import { DESCRIPTION, POPUP_CONTAINER, TITLE } from "./layout.const";
 import { Footer } from "./components/Footer/Footer";
 import { Providers } from "@/redux";
+import { Roboto } from "next/font/google";
 
 export const metadata = {
   title: TITLE,
   description: DESCRIPTION,
 };
+
+export const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin", "cyrillic"],
+  variable: "--font-roboto",
+});
 
 export default function RootLayout({
   children,
@@ -16,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.variable}>
       <body className={styles.body}>
         <Providers>
           <Header />
